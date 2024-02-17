45a0a300da041a6e58eae074fc6da0f0cb063208
S. T. Hoffmann <STHoffmann@protonmail.com>
2024-02-17 16:52:44 +0100

    docs: Added comments

--- solutions/binaryFile2/binaryFile2.cpp
+++ solutions/binaryFile2/binaryFile2.cpp
@@ -1,11 +1,29 @@
+/**
+ * @file binaryfile1.cpp
+ * @brief Beispielcode mit globalen Funktionen zum Speichern und Lesen von Strings in Binärdateien.
+ * @date 15.02.2024 – 17.02.2024
+ * @author Qualidy GmbH
+ * 
+ * /// Sind Erklärungen hinter Code um diesen zu erklären
+ * // Sind Erklärungen, die dem Code voran gehen.
+ * /* Leitet optionalen Code samt Erklärungen dazu innerhalb dessen ein.
+ * /** In solchen Blöcken stehen Zusammenfassungen Funktionen und Klassen.
+ */
 #include <fstream>
 #include <iostream>
-
+/**
+ * Templatefunktion um beliebige Datentypen binär zu speichern.
+ */
 template <typename T> void write_obj_binary(const T &obj, std::ostream &bos) {
   auto ptr = reinterpret_cast<const char *>(&obj);
   bos.write(ptr, sizeof(obj));
 }
 
+/**
+ * Spezialisierte Templatefunktion für T=std::string um diesen binär zu speichern.
+ * 
+ * Dazu wird erst die Anzahl zu speichernder Zeichen, dann die Zeichen selbst geschrieben.
+ */
 template <> void write_obj_binary(const std::string &str, std::ostream &bos) {
   int stringlength = str.size();
   auto slp = reinterpret_cast<char *>(&stringlength);
@@ -13,11 +31,20 @@ template <> void write_obj_binary(const std::string &str, std::ostream &bos) {
   bos.write(str.c_str(), stringlength);
 }
 
+/**
+ * Templatefunktion um beliebige Datentypen binär zu laden.
+ */
 template <typename T> void read_obj_binary(T &obj, std::istream &bis) {
   auto ptr = reinterpret_cast<char *>(&obj);
   bis.read(ptr, sizeof(obj));
 }
 
+
+/**
+ * Spezialisierte Templatefunktion für T=std::string um diesen binär zu laden.
+ * 
+ * Dazu wird erst die Anzahl Zeichen für den String geladen, dann die Zeichen selbst.
+ */
 template <> void read_obj_binary(std::string &str, std::istream &bis) {
   int stringlength = 0;
   read_obj_binary(stringlength, bis);
@@ -27,6 +54,9 @@ template <> void read_obj_binary(std::string &str, std::istream &bis) {
   str = buffer;
 }
 
+/**
+ * Eine Klasse mit verschiedenen Datentypen um die Speicherfunktionen zu zeigen
+ */
 struct Book {
   std::string title;
   std::string autor;
@@ -34,6 +64,12 @@ struct Book {
   int pages;
   double price;
 
+    /**
+     * Hilfsmemberfunktion, schreibt Stringdaten in output file stream.
+     * 
+     * `str` ist der Name der Datei in die geschrieben werden soll.
+     * `bos` ist der Ausgabestrom, in den geschrieben werden soll.
+     */
   void write_string_binary(const std::string &str, std::ostream &bos) const {
     int stringlength = str.size();
     auto slp = reinterpret_cast<char *>(&stringlength);
@@ -41,7 +77,18 @@ struct Book {
     bos.write(str.c_str(), stringlength);
   }
 
+    /**
+     * Memberfunktion, schreibt Buch-Objekt binär in Datei.
+     * 
+     * `filename` ist der Name der Datei in die geschrieben werden soll.
+     */
   void write_to_binary(const std::string &filename) const;
+  
+    /**
+     * Memberfunktion, liest Buch-Objekt aus Binärdatei.
+     * 
+     * `filename` ist der Name der Datei aus der gelesen werden soll.
+     */
   void read_from_binary(const std::string &filename);
 };
 
@@ -93,6 +140,9 @@ void Book::read_from_binary(const std::string &filename) {
   read_obj_binary(price, file);
 }
 
+/**
+ * Einstiegspunkt in Programm, schreibt `b1` in Datei und läd diese in `b2`.
+ */
 int main() {
   Book b1{"Achtung, C++", "Thomas Mahr", 2019, 460, 29.99};
   Book b2{"A", "B", 1, 1, 8.15};
