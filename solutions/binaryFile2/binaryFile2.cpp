1d81aa7ec61af70e085b4783140210a02a7088af
S. T. Hoffmann <STHoffmann@protonmail.com>
2024-02-18 12:57:27 +0100

    refactor: Improved code quality
    No in-class function bodies anymore, made member variables private to stress that only the binary read/write functions are able to modify an object. Removed non-standard variable length array and replaced with dynamic memory allocation technique. std::vector would still be better though ...

--- solutions/binaryFile2/binaryFile2.cpp
+++ solutions/binaryFile2/binaryFile2.cpp
@@ -11,30 +11,19 @@
 /**
  * Templatefunktion um beliebige Datentypen binär zu speichern.
  */
-template <typename T> void write_obj_binary(const T &obj, std::ostream &bos) {
-  auto ptr = reinterpret_cast<const char *>(&obj);
-  bos.write(ptr, sizeof(obj));
-}
+template <typename T> void write_obj_binary(const T &obj, std::ostream &bos);
 
 /**
  * Spezialisierte Templatefunktion für T=std::string um diesen binär zu speichern.
  * 
  * Dazu wird erst die Anzahl zu speichernder Zeichen, dann die Zeichen selbst geschrieben.
  */
-template <> void write_obj_binary(const std::string &str, std::ostream &bos) {
-  int stringlength = str.size();
-  auto slp = reinterpret_cast<char *>(&stringlength);
-  bos.write(slp, sizeof(stringlength));
-  bos.write(str.c_str(), stringlength);
-}
+template <> void write_obj_binary(const std::string &str, std::ostream &bos);
 
 /**
  * Templatefunktion um beliebige Datentypen binär zu laden.
  */
-template <typename T> void read_obj_binary(T &obj, std::istream &bis) {
-  auto ptr = reinterpret_cast<char *>(&obj);
-  bis.read(ptr, sizeof(obj));
-}
+template <typename T> void read_obj_binary(T &obj, std::istream &bis);
 
 
 /**
@@ -42,24 +31,23 @@ template <typename T> void read_obj_binary(T &obj, std::istream &bis) {
  * 
  * Dazu wird erst die Anzahl Zeichen für den String geladen, dann die Zeichen selbst.
  */
-template <> void read_obj_binary(std::string &str, std::istream &bis) {
-  int stringlength = 0;
-  read_obj_binary(stringlength, bis);
-  char buffer[stringlength + 1];
-  bis.read(buffer, stringlength);
-  buffer[stringlength] = '\0';
-  str = buffer;
-}
+template <> void read_obj_binary(std::string &str, std::istream &bis);
 
 /**
  * Eine Klasse mit verschiedenen Datentypen um die Speicherfunktionen zu zeigen
  */
-struct Book {
+class Book {
   std::string title;
   std::string autor;
   short release;
   int pages;
   double price;
+  public:
+  /**
+   * Konstruktor um alle Membervariablen zu initialisieren
+   */
+  Book(const std::string& _title, const std::string& _author, short _release, int _pages, double _price)
+      : title(_title), autor(_author), release(_release), pages(_pages), price(_price) {}
 
     /**
      * Hilfsmemberfunktion, schreibt Stringdaten in output file stream.
@@ -67,15 +55,12 @@ struct Book {
      * `str` ist der Name der Datei in die geschrieben werden soll.
      * `bos` ist der Ausgabestrom, in den geschrieben werden soll.
      */
-  void write_string_binary(const std::string &str, std::ostream &bos) const {
-    int stringlength = str.size();
-    auto slp = reinterpret_cast<char *>(&stringlength);
-    bos.write(slp, sizeof(stringlength));
-    bos.write(str.c_str(), stringlength);
-  }
+  void write_string_binary(const std::string &str, std::ostream &bos) const;
 
     /**
      * Memberfunktion, schreibt Buch-Objekt binär in Datei.
+     * 
+     * Nutzt dazu die globalen Funktionen für einzelne Member.
      * 
      * `filename` ist der Name der Datei in die geschrieben werden soll.
      */
@@ -87,12 +72,59 @@ struct Book {
      * `filename` ist der Name der Datei aus der gelesen werden soll.
      */
   void read_from_binary(const std::string &filename);
+  
+  /**
+   * Memberfunktion zur Ausgabe der Attribute
+   */
+   void print_summary() const;
 };
-/**
- * Memberfunktion um das gesamte Objekt in eine Binärdatei zu speichern.
- * 
- * Nutzt dazu die globalen Funktionen für einzelne Member.
- */
+
+/// Globale Funktionen
+template <typename T> void write_obj_binary(const T &obj, std::ostream &bos) {
+  const auto ptr = reinterpret_cast<const char *>(&obj);
+  
+  bos.write(ptr, sizeof(obj));
+}
+
+template <> void write_obj_binary(const std::string &str, std::ostream &bos) {
+  const int stringlength = str.size();
+  const auto slp = reinterpret_cast<const char *>(&stringlength);
+  
+  bos.write(slp, sizeof(stringlength));
+  bos.write(str.c_str(), stringlength);
+}
+
+template <typename T> void read_obj_binary(T &obj, std::istream &bis) {
+  auto ptr = reinterpret_cast<char *>(&obj);
+  
+  bis.read(ptr, sizeof(obj));
+}
+
+template <> void read_obj_binary(std::string &str, std::istream &bis) {
+  int stringlength = 0;
+  
+  read_obj_binary(stringlength, bis);
+  
+  auto buffer = new char[stringlength + 1];
+  
+  bis.read(buffer, stringlength);
+  
+  buffer[stringlength] = '\0';
+  str = buffer;
+  
+  delete[] buffer;
+}
+
+/// Memberfunktionen von Book
+
+  void Book::write_string_binary(const std::string &str, std::ostream &bos) const {
+    int stringlength = str.size();
+    auto slp = reinterpret_cast<char *>(&stringlength);
+    
+    bos.write(slp, sizeof(stringlength));
+    bos.write(str.c_str(), stringlength);
+  }
+  
 void Book::write_to_binary(const std::string &filename) const {
   std::ofstream file(filename, std::ios::binary);
 
@@ -145,9 +177,12 @@ void Book::read_from_binary(const std::string &filename) {
   // price auslesen
   read_obj_binary(price, file);
 }
+   void Book::print_summary() const {
+       std::cout << title << ' ' << autor << ' ' << pages << ' ' << release << ' ' << price << '\n';
+   }
 
 /**
- * Einstiegspunkt in Programm, schreibt `b1` in Datei und läd diese in `b2`.
+ * Einstiegspunkt in Programm, schreibt `b1` in Datei und läd Dateiinhalt in `b2`.
  */
 int main() {
   Book b1{"Achtung, C++", "Thomas Mahr", 2019, 460, 29.99};
@@ -156,8 +191,7 @@ int main() {
   b1.write_to_binary("Books.bin");
   b2.read_from_binary("Books.bin");
 
-  std::cout << b2.title << ' ' << b2.autor << ' ' << b2.pages << ' '
-            << b2.release << ' ' << b2.price << '\n';
+  b2.print_summary();
 
   return 0;
 }
